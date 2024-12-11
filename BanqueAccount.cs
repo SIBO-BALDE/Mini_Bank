e884994fb70b1f8a9297dcc1fee207442d8f1f23
SIBO BALDE <111776133+SIBO-BALDE@users.noreply.github.com>
2024-12-11 16:25:40 +0000

    Ajoutez des fichiers projet.

---
+++ BanqueAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nest;
+
+
+namespace Mini_Bank
+{
+    internal class BanqueAccount
+    {
+        
+        private double  Money =0;
+        private string  Name;
+
+       public BanqueAccount(double money, string name)
+        {
+            Money = money;
+            this.Name = name;
+
+        }
+        public void AddMoney(double amount)
+        {
+            Money += amount;
+       
+
+        }
+        public void Subtract(double money)
+        {
+            this.Money -= money;
+
+
+        }
+        public void GetMoney()
+        {
+            Console.WriteLine(Name + "has" + Money);
+        }
+
+
+        // Ajout de la méthode getInfo()
+        public void GetInfo()
+        {
+            Console.WriteLine($"{Name} a {Money:C} dans son compte");
+        }
+        
+
+      }
+
+    
+
+    }
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Mini_Bank // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BanqueAccount bankaccount = new BanqueAccount(money: 100, name: "Sibo Balde");
+            bankaccount.AddMoney(amount: 50);
+            bankaccount.GetInfo();
+            bankaccount.Subtract(money: 10);
+            bankaccount.GetInfo();
+        }
+    }
+}
